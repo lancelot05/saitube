53ff2c618f3242538fc699d3fc0a43427800757d
lancelot05 <53702551+lancelot05@users.noreply.github.com>
2020-03-21 14:05:33 +0530

    passed fav ids

--- favourites/models.py
+++ favourites/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# class Favourites(models.Model):
+#     video_id = models.CharField(max_length=11)
+#     video_thumbnail_url = models.CharField(max_length=50)
+    
--- favourites/views.py
+++ favourites/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render,redirect
 import requests
 
+fav_vids_ids = []
 # Create your views here.
-def favourites(request):
-	return render(request,'favourites/favourites.html')
+def favourites(request, id):
+	if request.method == 'POST':
+		fav_vids_ids.append(id)
+		print(fav_vids_ids)
+	context_dict = {
+		'favs' : fav_vids_ids,
+	}
+	return render(request,'favourites/favourites.html', context_dict)
